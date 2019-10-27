e4451a22cf91cd801ad6855236315fa5d405bd4d
willkendall01 <43126467+willkendall01@users.noreply.github.com>
2019-10-26 21:26:43 -0400

    Update customcookie.js

--- customcookie.js
+++ customcookie.js
@@ -5,6 +5,7 @@ var toppings = Array("Nothing","Vanilla Frosting", "Chocolate Frosting", "Maple 
 
 
 function parseForm() {
+
 	var dough = document.getElementById("dough");
 	var dough = doughs[dough.options[dough.selectedIndex].value];
 
@@ -13,6 +14,35 @@ function parseForm() {
 
 	var topping = document.getElementById("topping");
 	var topping = toppings[topping.options[topping.selectedIndex].value];
+	reformat(dough, fillIn, topping);
 
-	alert("" + dough + " " + fillIn + " " + topping)
+}
+
+function reformat(dough, fill, topp) {
+	$("#row").toggleClass("row", true);
+	$("#col").toggleClass("column", true);
+	$("#selections").toggleClass("selections", false)
+	$("#creation").html( namer(dough, fill, topp));
+}
+
+function namer(d,f,t){
+	var top_bool = (t === "Nothing");
+	var fill_bool = (f === "Nothing");
+
+
+
+	if (fill_bool && top_bool) {
+		return "No topping? No fillers? Are you sure? Well, we guess our dough is delicious! Enjoy your " + d + " Cookie!"
+	}
+	else if (!fill_bool && !top_bool ) {
+		return "Yay! You've made a " + f + " " + d + " Cookie with " + t + " on top. Sounds delicious!"
+	}
+	else if (fill_bool) {
+		return "Yay! You've made a " + d + " Cookie with " + t + " on top. Sounds delicious!"
+	}
+	else if (top_bool) {
+		return "Yay! You've made a " + f + " " + d + " Cookie. Sounds delicious!"
+	}
+
+
 }
